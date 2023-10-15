3e9c45e7185e868fa01a658306184466c17b66e0
Priyesh Mathur <PMathur@slb.com>
2023-10-15 20:15:33 +0530

    OCt9 Code

--- 9Oct/Lab0010.py
+++
@@ -1,8 +0,0 @@
-# Escape sequence
-
-print("This is a backslash \\")     # This is a backslash \
-print("She said: \"Hello, world!\"")        # She said: "Hello, world!"
-print("This is a new line \nThis is the second line")       #This is a new line
-print("This is a tab\tThis is after the tab")
-'''This is the second line
-This is a tab	This is after the tab'''
--- 9Oct/Lab008.py
+++
@@ -1,5 +0,0 @@
-val = None
-print(val)
-print(type(val))
-print(id(val))
-#print(len(val))     # TypeError: object of type 'NoneType' has no len()
